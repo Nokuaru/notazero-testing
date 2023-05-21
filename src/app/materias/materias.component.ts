d39e6d0537bf95895aaf54e224d7716106ca4d2a
Nokuaru <nokuaru@gmail.com>
2023-05-21 12:40:35 -0300

    Modificado el componente para que funcione con CORS

--- src/app/materias/materias.component.ts
+++ src/app/materias/materias.component.ts
@@ -15,33 +15,30 @@ export class MateriasComponent {
     let final = <HTMLInputElement>document.getElementById("final");
     
     let obj = {
-      id: materia,
-      price: parcial1,
-      name: parcial2,
+      
+      price: parcial1.value,
+      name: parcial2.value,
     }
-    fetch("https://qhhfu8cg5g.execute-api.us-east-1.amazonaws.com/items/1"), {
+    /*fetch("https://qhhfu8cg5g.execute-api.us-east-1.amazonaws.com/items/1"), {
       headers: {
         "Access-Control-Allow-Origin": "*", // Required for CORS support to work
         "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
-      }}
+      },
+    
+    }
     
 
-
-
-
-
-    /*
+*/
     fetch("https://qhhfu8cg5g.execute-api.us-east-1.amazonaws.com/items", {
       method:'PUT',
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'   
+        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
       },
-      mode:'no-cors',
+      
       body: JSON.stringify(obj)
       })
       .then(response => response.json())
       .then(response => console.log(JSON.stringify(response)))
-      .
-      */
+
+      
 }}
