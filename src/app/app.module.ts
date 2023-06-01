10ab021b1788daf65e300c73dbf18cd785216cd2
Nokuaru <nokuaru@gmail.com>
2023-05-31 22:32:17 -0300

    Agregado yaml para Amplify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MateriasComponent } from './materias/materias.component';
-import { GetpostService } from './getpost.service'
+import { GetpostService } from './getpost.service';
+import { LoggedInComponent } from './logged-in/logged-in.component';
+import { LoggedOutComponent } from './logged-out/logged-out.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     MateriasComponent,
+    LoggedInComponent,
+    LoggedOutComponent,
     
   ],
   imports: [
